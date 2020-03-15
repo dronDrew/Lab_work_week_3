619d6e5427e73961a0b90e4f394760aa2174d7ee
Андрій Дудчак <andrew.melauda@gmail.com>
2020-03-15 16:59:57 +0300

    task1_fin

--- Lab_work_week_3/Program.cs
+++ Lab_work_week_3/Program.cs
@@ -10,6 +10,20 @@ namespace Lab_work_week_3
     {
         static void Main(string[] args)
         {
+            Range_array<string> A= new Range_array<string>();
+            Range_array<string> B= new Range_array<string>(-9,6);
+            Range_array<string> C = new Range_array<string>(1, 10,"Freank");
+            C[2] = "John";
+            C[9] = "alphread";
+            B[-5] = "Greag";
+            B[5] = "olaf";
+            Console.WriteLine(C[1]);
+            Console.WriteLine(C.Lenght());
+            Console.WriteLine(B.Lenght());
+            A.SetSize(10, 20);
+            Console.WriteLine(A.Lenght());
+            Console.WriteLine(A.GetLowerBound());
+            Console.WriteLine(A.GetUpperBound());
         }
     }
 }
--- Lab_work_week_3/Range_array.cs
+++ Lab_work_week_3/Range_array.cs
@@ -6,9 +6,66 @@ using System.Threading.Tasks;
 
 namespace Lab_work_week_3
 {
-    class Range_array
+    class Range_array<T>
     {
         private int appertBound;
         private int lowerBound;
+        private T[] arr;
+        public Range_array() {
+            this.lowerBound = 0;
+            this.appertBound = 0;
+            this.arr = null;
+        }
+        public Range_array(int LowerB,int UpperB) {
+            if (UpperB < LowerB) {
+                int res = LowerB;
+                LowerB = UpperB;
+                UpperB = res;
+            }
+            this.lowerBound = LowerB;
+            this.appertBound = UpperB;
+            this.arr = new T[appertBound - lowerBound];
+        }
+        public Range_array(int LowerB, int UpperB,T val)
+        {
+            if (UpperB < LowerB)
+            {
+                int res = LowerB;
+                LowerB = UpperB;
+                UpperB = res;
+            }
+            this.lowerBound = LowerB;
+            this.appertBound = UpperB;
+            this.arr = new T[appertBound - lowerBound];
+            arr[0] = val;
+        }
+        public T this[int i] {
+            get { 
+                    return this.arr[i - this.lowerBound];
+              
+                    }
+            set { this.arr[i-lowerBound] = value; }
+        }
+        public int Lenght() {
+            return this.appertBound - this.lowerBound;
+        }
+        public void SetSize(int LowerB, int UpperB) {
+            if (UpperB < LowerB)
+            {
+                int res = LowerB;
+                LowerB = UpperB;
+                UpperB = res;
+            }
+            this.lowerBound = LowerB;
+            this.appertBound = UpperB;
+            this.arr = new T[appertBound - lowerBound];
+        }
+        public int GetLowerBound() {
+            return this.lowerBound;
+        }
+        public int GetUpperBound()
+        {
+            return this.appertBound;
+        }
     }
 }
